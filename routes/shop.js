d73351b63e6a9bb0384cbe992ff5626aae933600
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2022-08-01 19:36:41 +0530

    Add files via upload

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const rootDir = require('../util/path');
+
+const path = require('path');
+
 const router = express.Router();
 
 router.get('/', (req,res,next) => {
-    res.send('<h1>Hello from Express.js</h1>');
+   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
    });
 
 module.exports = router;

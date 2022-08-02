c55a824c2ab25e5c81bc32aa40342e7fe1cffd88
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2022-08-02 09:01:29 +0530

    Add files via upload

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 
-const rootDir = require('../util/path');
-
-const path = require('path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/add-product',(req, res, next ) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  });
-  
-  router.post('/add-product', (req, res, next) => {
-  
-    console.log(req.body);
-    res.redirect('/');
-  });
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;

9efa6087597678e0a2eafab20180f8329e8f22ee
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2022-09-23 12:08:39 +0530

    Add files via upload

--- models/product.js
+++ models/product.js
@@ -1,30 +1,29 @@
-const db = require('../util/database');
-const Cart = require('./cart');
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
 
+const Product = sequelize.define('product',{
+     id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+     },
+     title: Sequelize.STRING,
+     price: {
+      type: Sequelize.DOUBLE,
+      allowNull: false
+     },
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+     imageUrl: {
+      type: Sequelize.STRING,
+      allowNull: false
+     },
 
-  save() {
-    return db.execute('INSERT INTO products (title,price,imageurl, description) VALUES (?, ?, ?, ?)',
-    [this.title, this.price, this.imageUrl, this.description]);
-  }
+     description: {
+      type: Sequelize.STRING,
+      allowNull: false
+     }
 
-  static deleteById(id) {
-    
-  }
+});
 
-  static fetchAll() {
-   return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-   return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+module.exports = Product;

cbaf39ee8eac4bc52da1b1c2f51de6511e96b4b3
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2022-09-23 16:38:34 +0530

    Add files via upload

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root','212005', {
+const sequelize = new Sequelize('node-complete', 'root', '212005', {
   dialect: 'mysql',
   host: 'localhost'
 });
 
 module.exports = sequelize;
-
